505d02921469f7634fd12d1f2faa6049f5e9d971
Natali Perebeynos <perebeynos-98@mail.ru>
2026-03-23 22:39:30 +0300

    Второй коммит

---
+++ src/pages/ToDoListPage.tsx
@@ -0,0 +1,13 @@
+import { Form } from "../components/Form/Form"
+import { Header } from "../components/Header/Header"
+import { ToDOList } from "../components/ToDoList/ToDOList"
+
+export const ToDoListPage = () => {
+    return (
+    <>
+    <Header />
+    <Form />
+    <ToDOList />
+        </>
+    )
+}
